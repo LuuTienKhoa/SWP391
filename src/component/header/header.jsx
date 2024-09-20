e04032bb35ca47200e48abd8e4557485d1bc0602
LufkyMama <163436312+LufkyMama@users.noreply.github.com>
2024-09-20 16:30:22 +0700

    Update header.jsx

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -2,21 +2,27 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 function Header() {
   return (
-    <header>
-      <ul>
+    <header className="flex justify-between items-center p-4 bg-orange-800"> {/* Flex container */}
+      <div className="flex items-center"> {/* Logo container */}
+        <img src="/path/to/logo.png" alt="Logo" className="text-white h-8 w-auto mr-4" /> {/* Adjust path and size */}
+      </div>
+      <ul className="flex space-x-12 mx-auto"> {/* Centered links */}
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" className="text-orange-200 hover:underline">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" className="text-orange-200 hover:underline">About</Link>
         </li>
         <li>
-          <Link to="/prodcuts">Shop</Link>
+          <Link to="/products" className="text-orange-200 hover:underline">Shop</Link>
         </li>
         <li>
-          <Link to="/login">Login</Link>
+          <Link to="/question" className="text-orange-200 hover:underline">FAQ</Link>
         </li>
       </ul>
+      <div>
+        <Link to="/login" className="text-orange-200 hover:underline">Login</Link> {/* Right-aligned Login */}
+      </div>
     </header>
   )
 }
