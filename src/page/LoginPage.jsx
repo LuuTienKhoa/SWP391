980d891ee8f1581829c6ccc04a6c19b78cf4e520
LuuTienKhoa <tienkhoa200604@gmail.com>
2024-09-20 16:30:40 +0700

    Update LoginPage.jsx

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react';
-import api from '../config/axios';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import api from "../config/axios";
+import { useNavigate } from "react-router-dom";
+import Cover_Image from "../assets/bg.jpg";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Email:', email);
-    console.log('Password:', password);
+    console.log("Email:", email);
+    console.log("Password:", password);
     // Add your authentication logic here
   };
   const navigate = useNavigate();
@@ -33,63 +34,81 @@ const LoginPage = () => {
 
   return (
     <>
-    <div>
-      <img src='../assets/images/bg.jpg' alt='logo'/>
-    </div>
-    
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
-        <h2 className="text-2xl font-bold text-center mb-5">Login</h2>
-        <h2 className="text-l font-bold text-center mb-2 text-gray-500">Welcome to F Koi Shop</h2>
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
+      <div className="w-full h-screen flex items-start">
+        <div className="relative w-1/2 h-full flex flex-col">
+          <img src={Cover_Image} alt="Cover" className="w-full h-full" />
+        </div>
+
+        <div className="min-h-screen relative w-1/2 flex flex-col items-center justify-center bg-gray-100">
+          <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
+            <h2 className="text-2xl font-bold text-center mb-5">Login</h2>
+            <h2 className="text-l font-bold text-center mb-2 text-gray-500">
+              Welcome to F Koi Shop
+            </h2>
+            <form onSubmit={handleSubmit} className="space-y-6">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  />
+                  <span className="ml-2 block text-sm text-gray-900">
+                    Remember me
+                  </span>
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-medium text-rose-600 hover:text-rose-700"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-rose-500 hover:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
           </div>
-          <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <label className="flex items-center">
-              <input type="checkbox" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
-              <span className="ml-2 block text-sm text-gray-900">Remember me</span>
-            </label>
-            <div className="text-sm">
-              <a href="#" className="font-medium text-rose-600 hover:text-rose-700">
-                Forgot your password?
-              </a>
-            </div>
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-rose-500 hover:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Sign in
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
-    </div>
     </>
   );
 };
