b50c9557b572368ff93bd7b678288ff8502ebc1d
LuuTienKhoa <tienkhoa200604@gmail.com>
2024-09-24 01:28:21 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <GoogleOAuthProvider clientId="<841740393347-on6id8tmaiqg89lr9fukbpma0h9h1uk8.apps.googleusercontent.com
+>">
       <App />
+      </GoogleOAuthProvider>
     </BrowserRouter>   
   </StrictMode>,
 )
