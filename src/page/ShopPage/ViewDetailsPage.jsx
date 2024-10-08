61d687856917135dd06af63f89adcbc27a9c37e2
LuuTienKhoa <tienkhoa200604@gmail.com>
2024-10-08 21:59:56 +0700

    Update ViewDetailsPage.jsx

--- src/page/ShopPage/ViewDetailsPage.jsx
+++ src/page/ShopPage/ViewDetailsPage.jsx
@@ -1,70 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from "react-router-dom";
+import { products } from "../../constants";   
 
 // Sample product data (replace with actual data fetching)
-const productData = [
-  {
-    id: '1',
-    title: "Black and White Koi",
-    price: 249.99,
-    description: "Elegant black and white patterned Koi",
-    image:"https://www.kodamakoifarm.com/wp-content/uploads/2024/08/w0812s001-260x421.jpg",
-    color: "black",
-    size: "large",
-    category: "premium",
-    popularity: 4.8,
-  },//1
-  {
-    id: '2',
-    title: "Orange Koi Fish",
-    price: 199.99,
-    description: "Vibrant orange Koi fish with white spots",
-    image:"https://www.kodamakoifarm.com/wp-content/uploads/2024/05/w0508s002.jpg",
-    color: "orange",
-    size: "medium",
-    category: "ornamental",
-    popularity: 4.5,
-  },//2
-  
-  {
-    id: '3',
-    title: "Red and White Koi",
-    price: 179.99,
-    description: "Beautiful red and white Koi fish fron VietNam",
-    image: "https://www.kodamakoifarm.com/wp-content/uploads/2024/08/w0826s001-re-260x421.jpg",
-    color: "red",
-    size: "small",
-    category: "standard",
-    popularity: 4.2,
-  },//3
-  {
-    id: '4',
-    title: "Red Koi",
-    price: 179.99,
-    description: "Beautiful red and white Koi fish from japan",
-    image:"https://www.kodamakoifarm.com/wp-content/uploads/2024/08/w0826s005-re-260x421.jpg",
-    color: "red",
-    size: "small",
-    category: "standard",
-    popularity: 4.2,
-  },//4
-  {
-    id: '5',
-    title: "Brown Koi",
-    price: 179.99,
-    description: "Beautiful red and white Koi fish from japan",
-    image:"https://www.kodamakoifarm.com/wp-content/uploads/2024/05/w0506s089-260x421.jpg",
-    color: "brown",
-    size: "small",
-    category: "standard",
-    popularity: 4.2,
-  },//5
-];
+
 
 const ProductDetailPage = () => {
   const { id } = useParams();
-  const product = productData.find((p) => p.id === id);
+  const product = products.find((p) => p.id === id);
 
   if (!product) {
     return <div>Product not found</div>;
