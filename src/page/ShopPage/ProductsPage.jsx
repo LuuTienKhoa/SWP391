f41e4ed48e8fa0bbc450c5fcf8306126211aac0d
LuuTienKhoa <tienkhoa200604@gmail.com>
2024-10-12 16:22:25 +0700

    Update ProductsPage.jsx

--- src/page/ShopPage/ProductsPage.jsx
+++ src/page/ShopPage/ProductsPage.jsx
@@ -1,23 +1,54 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import api from "../../config/axios";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+import Button from "../../component/Button";
+import ShoppingCart from "../../component/ShoppingCart";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 
-const KoiFishProducts = () => {
+const ProductsPage = () => {
   const [koiFishs, setKoiFishs] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [sortOrder, setSortOrder] = useState("asc");
-  const [selectedColors, setSelectedColors] = useState([]); // State for color filter
-  const [ageRange, setAgeRange] = useState([0, 10]); // State for age filter
-  const [priceRange, setPriceRange] = useState([0, 1000]); // State for price filter
+  const [ageRange, setAgeRange] = useState([0, 10]);
+  const [priceRange, setPriceRange] = useState([0, 1000]);
+  const [sizeRange, setSizeRange] = useState([0, 100]);
+  const [color, setColor] = useState("");
+  const [cartOpen, setCartOpen] = useState(false);
+  const [filterVisible, setFilterVisible] = useState(false); // State for filter visibility
   const location = useLocation();
   const navigate = useNavigate();
 
   const fetchKoiFish = async () => {
     try {
-      const response = await api.get("koi/Koi");
+      let endpoint = "/koi/Koi/sorted";
+      const params = new URLSearchParams({
+        sortOrder: sortOrder,
+        minPrice: priceRange[0].toString(),
+        maxPrice: priceRange[1].toString(),
+        minAge: ageRange[0].toString(),
+        maxAge: ageRange[1].toString(),
+        minSize: sizeRange[0].toString(),
+        maxSize: sizeRange[1].toString(),
+      });
+
+      if (color) {
+        params.append("color", color);
+      }
+
+      if (searchQuery) {
+        endpoint = "/koi/Koi/Search";
+        params.append("koiFish.name", searchQuery);
+      }
+
+      const response = await api.get(`${endpoint}?${params.toString()}`);
       setKoiFishs(response.data);
     } catch (error) {
       console.error("Error fetching koi fish:", error);
+      alert(
+        "Failed to fetch koi fish. Please check the console for more details."
+      );
     }
   };
 
@@ -25,174 +56,196 @@ const KoiFishProducts = () => {
     if (location.pathname === "/products") {
       fetchKoiFish();
     }
-  }, [location]);
+  }, [
+    location,
+    searchQuery,
+    sortOrder,
+    ageRange,
+    priceRange,
+    sizeRange,
+    color,
+  ]);
 
   const handleAddToCart = async (koiFish) => {
     try {
       const response = await api.post("cart", {
-        productId: koiFish.id,
+        productId: koiFish.koiID,
         quantity: 1,
       });
       console.log(response);
     } catch (err) {
       console.log(err);
-      alert("Thêm vào giỏ hàng thất bại!");
+      alert("Failed to add to cart!");
     }
   };
 
-  const handleColorChange = (color) => {
-    setSelectedColors((prevSelectedColors) =>
-      prevSelectedColors.includes(color)
-        ? prevSelectedColors.filter((c) => c !== color)
-        : [...prevSelectedColors, color]
-    );
-  };
-
-  // Updated filtering logic to handle age and price
-  const filteredAndSortedKoiFishs = useMemo(() => {
-    return koiFishs
-      .filter((koiFish) =>
-        koiFish.name.toLowerCase().includes(searchQuery.toLowerCase())
-      )
-      .filter((koiFish) => {
-        const koiColors = koiFish.color.toLowerCase().split(", "); // Split the color string into an array
-        return selectedColors.length === 0 || selectedColors.some(color => koiColors.includes(color));
-      })
-      .filter((koiFish) => koiFish.age >= ageRange[0] && koiFish.age <= ageRange[1]) // Filter by age
-      .filter((koiFish) => koiFish.price >= priceRange[0] && koiFish.price <= priceRange[1]) // Filter by price
-      .sort((a, b) =>
-        sortOrder === "asc" ? a.price - b.price : b.price - a.price
-      );
-  }, [koiFishs, searchQuery, selectedColors, sortOrder, ageRange, priceRange]);
-
-  const availableColors = ["red", "white", "black", "yellow", "blue", "orange"];
+  const availableColors = ["red", "black", "orange", "white", "blue", "yellow"];
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <div className="w-full text-5xl font-semibold mb-2 flex flex-col items-center">
-        <h1>F Koi Shop</h1>
-      </div>
-      
-      <div className="flex justify-between items-center mb-4">
-        
-        <input
-          type="text"
-          placeholder="Search koi fish by name"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="mb-4 p-2 border border-gray-300 rounded"
-        />
-
-        
-      </div>
-
-      <div className="mb-4">
-        <span className="font-bold">Filter by Color:</span>
-        <div className="flex space-x-4 mt-2">
-          {availableColors.map((color) => (
-            <label key={color} className="flex items-center space-x-2">
-              <input
-                type="checkbox"
-                value={color}
-                onChange={() => handleColorChange(color)}
-                checked={selectedColors.includes(color)}
-              />
-              <span className="capitalize">{color}</span>
-            </label>
-          ))}
+    <div className="bg-orange-100 min-h-screen">
+      <div className="container mx-auto px-4 py-7">
+        <div className="w-full text-5xl mb-2 flex flex-col items-center">
+          <h1>F Koi Shop</h1>
         </div>
-      </div>
-<div>
-      <div className="mb-4 ">
-        <span className="font-bold">Filter by Age:    </span>
-        <input
-          type="range"
-          min="0"
-          max="10"
-          value={ageRange[0]}
-          onChange={(e) => setAgeRange([Number(e.target.value), ageRange[1]])}
-        />
-        <input
-          type="range"
-          min="0"
-          max="10"
-          value={ageRange[1]}
-          onChange={(e) => setAgeRange([ageRange[0], Number(e.target.value)])}
-        />
-        <span>{ageRange[0]} - {ageRange[1]} years</span>
-      </div>
 
-      <div className="mb-4 ">
-        <span className="font-bold">Filter by Price: </span>
-        <input
-          type="range"
-          min="0"
-          max="1000"
-          value={priceRange[0]}
-          onChange={(e) => setPriceRange([Number(e.target.value), priceRange[1]])}
-        />
-        <input
-          type="range"
-          min="0"
-          max="1000"
-          value={priceRange[1]}
-          onChange={(e) => setPriceRange([priceRange[0], Number(e.target.value)])}
-        />
-        <span>${priceRange[0]} - ${priceRange[1]}</span>
-      </div>
-</div>
-
-      <div className="mb-4 flex justify-end">
-        <select
-          value={sortOrder}
-          onChange={(e) => setSortOrder(e.target.value)}
-          className="p-2 bg-gray-200 rounded"
-        >
-          <option value="asc">Sort by Price: Ascending</option>
-          <option value="desc">Sort by Price: Descending</option>
-          <option value="latest">Sort by Latest</option>
-        </select>
-      </div>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-        {filteredAndSortedKoiFishs.map((koiFish) => (
-          <div
-            key={koiFish.koiID}
-            className="bg-white rounded-lg shadow-md overflow-hidden"
-          >
-            <img
-              src={koiFish.image ?? "https://www.kodamakoifarm.com/wp-content/uploads/2024/09/w0911s005-re-260x421.jpg"}
-              alt={koiFish.name}
-              className="w-full h-100 object-cover"
+        <div className="flex justify-between items-center mb-8 p-4">
+          {/* Search by Title */}
+          <div className="flex-1 mr-4">
+            <input
+              type="text"
+              placeholder="Search koi fish by name"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded"
             />
-            <div className="p-4">
-              <Link to={`/view-details/${koiFish.koiID}`}>
-                <h2 className="text-xl font-semibold mb-2">{koiFish.name}</h2>
-              </Link>
-              <span className="text-2xl font-bold text-blue-600">
-                ${koiFish.price.toFixed(2)}
-              </span>
-              <div className="flex justify-between items-center mt-4">
-                <button
-                  onClick={() => handleAddToCart(koiFish)}
-                  className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-300"
-                >
-                  Add to Cart
-                </button>
-              
-                <button
-                  onClick={() => navigate("/payment")}
-                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-300"
-                >
-                  Payment
-                </button>
+          </div>
+
+          {/* Buttons */}
+          <div className="flex space-x-4">
+            <Button onClick={() => setFilterVisible(!filterVisible)} className="bg-orange-200 px-4 py-2 rounded">
+              Filter
+            </Button>
+            <button
+              onClick={() => setCartOpen(true)}
+              className="flex items-center justify-center bg-white border border-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-100 transition duration-300"
+            >
+              <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+
+        {/* Filter Section */}
+        
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mb-8 p-4 border rounded-lg bg-gray-50">
+            {/* Search by Color */}
+            <div>
+              <label className="font-bold">Search by color:</label>
+              <select
+                value={color}
+                onChange={(e) => setColor(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded"
+              >
+                <option value="">All Colors</option>
+                {availableColors.map((c) => (
+                  <option key={c} value={c}>
+                    {c}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            {/* Search by Size */}
+            <div>
+              <label className="font-bold">Search by size:</label>
+              <Slider
+                range
+                min={0}
+                max={100}
+                defaultValue={[0, 100]}
+                value={sizeRange}
+                onChange={(value) => setSizeRange(value)}
+                className="mb-4"
+              />
+              <div className="flex justify-between text-sm">
+                <span>{sizeRange[0]} cm</span>
+                <span>{sizeRange[1]} cm</span>
+              </div>
+            </div>
+
+            {/* Search by Price */}
+            <div>
+              <label className="font-bold">Search by price:</label>
+              <Slider
+                range
+                min={0}
+                max={1000}
+                defaultValue={[0, 1000]}
+                value={priceRange}
+                onChange={(value) => setPriceRange(value)}
+                className="mb-4"
+              />
+              <div className="flex justify-between text-sm">
+                <span>{priceRange[0]} $</span>
+                <span>{priceRange[1]} $</span>
               </div>
             </div>
           </div>
-        ))}
+        
+
+        {/* Sort */}
+        <div className="flex justify-end mb-3">
+          <select
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value)}
+            className="p-2 border border-gray-300 rounded"
+          >
+            <option value="asc">Price: Ascending</option>
+            <option value="desc">Price: Descending</option>
+          </select>
+        </div>
+
+        {/* Product Grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          {koiFishs.map((koiFish) => (
+            <div key={koiFish.koiID} className="max-w-xs overflow-hidden">
+              <Link to={`/view-details/${koiFish.koiID}`}>
+                <img
+                  src={
+                    koiFish.image ??
+                    "https://www.kodamakoifarm.com/wp-content/uploads/2024/05/w0503s054-re-260x421.jpg"
+                  }
+                  alt={koiFish.name}
+                  className="w-full h-100 object-cover"
+                />
+              </Link>
+              <div className="p-4">
+                <h1 className="text-lg font-semibold">{koiFish.name}</h1>
+                <p className="text-sm text-gray-500">#{koiFish.koiID}</p>
+                <p className="text-red-500 font-bold mt-2">
+                  Price: ${koiFish.price.toFixed(2) || "N/A"}
+                </p>
+
+                <div className="mt-4">
+                  <p className="text-sm">
+                    <strong>Age:</strong> {koiFish.age || "Unknown"}
+                  </p>
+                  <p className="text-sm">
+                    <strong>Sex:</strong> {koiFish.gender || "Unknown"}
+                  </p>
+                  <p className="text-sm">
+                    <strong>Size:</strong> {koiFish.size || "Unknown"}
+                  </p>
+                  <p className="text-sm">
+                    <strong>Color:</strong> {koiFish.color || "Unknown"}
+                  </p>
+                  <p className="text-red-500 text-sm">
+                    <strong>Specie:</strong> {koiFish.species || "Unknown"}
+                  </p>
+                </div>
+                <div className="flex justify-between items-center mt-4">
+                  <button
+                    onClick={() => handleAddToCart(koiFish)}
+                    className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-300"
+                  >
+                    Add to Cart
+                  </button>
+                  <button
+                    onClick={() => navigate("/payment")}
+                    className="bg-white text-red-500 border border-red-500 px-4 py-2 rounded hover:bg-red-100 transition duration-300"
+                  >
+                    Pay Now
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* Shopping Cart Dialog */}
+        <ShoppingCart open={cartOpen} setOpen={setCartOpen} products={koiFishs} />
       </div>
     </div>
   );
 };
 
-export default KoiFishProducts;
+export default ProductsPage;
