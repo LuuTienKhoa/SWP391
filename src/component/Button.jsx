c4953ca79bed1dd6e86305be9c9640036b6e74a9
LuuTienKhoa <tienkhoa200604@gmail.com>
2024-10-15 01:44:33 +0700

    Update Button.jsx

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-const Button = () => {
+const Button = ({ onClick, filterVisible }) => {
   return (
-    <button className="text-white relative px-8 py-2 rounded-md bg-orange-200 isolation-auto z-10 border-2 border-lime-500\n        before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-orange-500 before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700">
+    <button
+      onClick={onClick}
+      className={`text-white relative px-8 py-2 rounded-md isolation-auto z-10 border-2  ${
+        filterVisible ? "bg-orange-600" : "bg-orange-400"
+      } before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-right-full before:hover:right-0 before:rounded-full  before:bg-orange-500 before:-z-10  before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700`}
+    >
       Filter
     </button>
   );
