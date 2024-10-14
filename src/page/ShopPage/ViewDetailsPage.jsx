9f2df81f31f58c48fa88f570f4c26656e13edf5f
LuuTienKhoa <tienkhoa200604@gmail.com>
2024-10-15 01:44:42 +0700

    Update ViewDetailsPage.jsx

--- src/page/ShopPage/ViewDetailsPage.jsx
+++ src/page/ShopPage/ViewDetailsPage.jsx
@@ -22,18 +22,18 @@ const ViewDetailsPage = () => {
   if (!koiFish) return <div>Loading...</div>;
 
   return (
-    <div className="container mx-auto p-4 ">
-      <div className="bg-white rounded-lg shadow-lg p-6 md:flex md:space-x-8">
-        <div className="md:w-1/2 flex flex-col justify-bewteen items-center ">
-        <img 
-          src={koiFish.image ?? "https://www.kodamakoifarm.com/wp-content/uploads/2024/05/w0503s054-re-260x421.jpg"
-          }
-          alt={koiFish.name}
-          className="w-full h-auto max-w-xs object-cover rounded-md "
+    <div className="container mx-auto p-4">
+      <div className="bg-white rounded-lg shadow-lg p-6 flex space-x-8">
+        <div className="w-1/2 flex justify-center items-center">
+          <img
+            src={koiFish.image ?? "https://www.kodamakoifarm.com/wp-content/uploads/2024/05/w0503s054-re-260x421.jpg"}
+            alt={koiFish.name}
+            className="w-full h-auto max-w-xs object-cover rounded-md"
           />
-          </div>
-        <div className="p-4">
-        <h2 className="text-3xl font-semibold mb-2">{koiFish.name} - #{koiFish.koiID}</h2>
+        </div>
+        <div className="w-1/2">
+          <h1 className="text-3xl mb-5">{koiFish.name} - #{koiFish.koiID}</h1>
+          <p className="text-2xl text-red-600 mb-5">${koiFish.price.toFixed(2)}</p>
           <p className="text-xl"><strong>Gender:</strong> {koiFish.gender}</p>
           <p className="text-xl"><strong>Age:</strong> {koiFish.age} years</p>
           <p className="text-xl"><strong>Size:</strong> {koiFish.size}</p>
@@ -42,12 +42,9 @@ const ViewDetailsPage = () => {
           <p className="text-xl"><strong>Personality:</strong> {koiFish.personality}</p>
           <p className="text-xl"><strong>Origin:</strong> {koiFish.origin}</p>
           <p className="text-xl"><strong>Selection Rate:</strong> {koiFish.selectionRate}</p>
-          <p className="text-xl"><strong>Species:</strong> {koiFish.species}</p>
-          
-          <p className="text-2xl font-bold text-red-600 bg-yellow-200 p-2 rounded shadow-lg">Price: ${koiFish.price.toFixed(2)}</p>
-          </div>
-        <div>
-          
+          <p className="text-xl text-red-600"><strong>Species:</strong> {koiFish.species}</p>
+          <button className="mt-4 px-4 py-2 bg-red-500 text-white rounded">Add to cart</button>
+          <button className="mt-2 px-4 py-2 bg-blue-500 text-white rounded">Payment</button>
         </div>
       </div>
     </div>
