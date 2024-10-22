d036759441ae694ad4f4789e2437e7b14d96ee29
LuuTienKhoa <tienkhoa200604@gmail.com>
2024-10-23 00:19:57 +0700

    Update StaffPage.jsx

--- src/page/StaffPage.jsx
+++ src/page/StaffPage.jsx
@@ -51,58 +51,7 @@ export default function AdminPage() {
 
   return (
     <div className="flex">
-      {/* Sidebar */}
-      {/* <div className="w-64 flex flex-col bg-gray-800 text-white p-4 h-screen">
-        <h2 className="text-2xl font-semibold mb-8 text-center">F Koi Staff            
-        </h2>
-        <ul>
-          {[
-            { label: 'Dashboard', icon: <MdDashboard size={24} />, link:'/' },
-            { label: 'Manage Koi', icon: <FaFish size={24} />, link:'manageKoi'},
-            { label: 'Manage Batch', icon: <FaFish size={24} />, link:'manageKoiBatch'},
-            { label: 'Manage Orders', icon: <FaShoppingCart size={24} />, link:'manageOrder' },
-          ].map((item, index) => (  
-            <li key={index} className="mb-4">
-              <Link to={item.link}>
-              <Button
-                startIcon={
-                  <Avatar
-                    sx={{
-                      backgroundColor: '#0288d1',
-                      height: 48,
-                      width: 48,
-                    }}
-                  >
-                    {item.icon}
-                  </Avatar>
-                }
-              >
-                {item.label}
-              </Button>
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <div className="mt-auto">
-          <Button 
-            color="inherit"
-            startIcon={
-              <Avatar
-                sx={{
-                  backgroundColor: '#0288d1',
-                  height: 48,
-                  width: 48,
-                }}
-              >
-              </Avatar>
-            }
-            className="mt-4"
-          >
-            Return
-          </Button>
-          
-        </div>
-      </div> */}
+      
 
       {/* Main Content */}
       <div className="flex-1 p-8 overflow-hidden">
